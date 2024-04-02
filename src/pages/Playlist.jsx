e9be99ff5e2423d0ad3c4903076de7f155f41722
Chris Sampson <csamposn@gmail.com>
2024-04-01 20:12:08 -0400

    added playlist table

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -3,8 +3,28 @@ import RequireUser from '../lib/RequireUser';
 import pb from '../lib/pocketclient';
 import { useNavigate } from 'react-router-dom';
 import {Button} from '../components/ui/button'
+import {Table, TableBody, TableCell, TableHeader, TableHead, TableRow} from "../components/ui/table";
+import {Card, CardContent, CardHeader, CardTitle, CardDescription} from '../components/ui/card';
+
+
+
+function TableItem({name, song}){
+    return (
+        <TableRow>
+            <TableCell>
+                {name}
+            </TableCell>
+            <TableCell>
+                {song}
+            </TableCell>
+        </TableRow>
+    )
+}
+
 
 export default function Playlist() {
+    const navigate = useNavigate()
+
     const [songs, setSongs] = useState([]);
     const [error, setError] = useState('');
 
@@ -45,30 +65,49 @@ export default function Playlist() {
         getSongs();
     }, []);
 
-    // TODO: create a nice table for this data to live in
-
     return (
         <RequireUser>
             {
                 error && 
                 <span className="text-red-400">{error}</span>
             }
-            <div>
-                { !error &&
-                <h3 className="text-[1.25em]">
-                    Yeah I brought the Jams. You're welcome.
-                </h3>
-                }
-                <div className="grid place-items-center my-6 py-6 border border-spacing-1 rounded-lg">
+            <Card>
+                <CardHeader>
+                    <div className="flex justify-start">
+                        <Button onClick={() => navigate('/dashboard')}>
+                            Back
+                        </Button>
+                    </div>
+                    { !error &&
+                    <>
+                        <CardTitle>
+                            Yeah I brought the Jams. You're welcome.
+                        </CardTitle>
+                        <CardDescription className="">
+                            The people has spoken
+                        </CardDescription>
+                    </>  
+                    }
+                </CardHeader>
+                <CardContent className=" m-5 grid place-items-center my-6 py-6 border border-spacing-1 rounded-lg">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableCell>
+                                    Submitted By
+                                </TableCell>
+                                <TableCell>
+                                    Song Request
+                                </TableCell>
+                            </TableRow>
+                        </TableHeader>
                     {items &&
                         items.map((e,i) => (
-                            <div className=" flex w-1/3 border justify-between" key={e.song + i}>
-                                <h4>{e.name}</h4>
-                                <h4>{e.song}</h4>
-                            </div>
+                            <TableItem song={e.song} name={e.name} />
                         ))}
-                </div>
-            </div>
+                    </Table>
+                </CardContent>
+            </Card>
         </RequireUser>
     );
 }
