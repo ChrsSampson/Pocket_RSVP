ed3d17ba656d39957878af5549f8f149f7e04727
Chris Sampson <chris@Digitell.Local>
2024-04-17 13:25:26 -0400

    fixes __dirname in production server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = 8080
 
 const app = express()
 
+const __dirname = path.resolve()
 
 // make sure dist exists
 const exists = fs.readdirSync('.').includes('dist')
@@ -19,8 +20,22 @@ app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
     res.sendFile('dist/index.html')
-    })
+})
+
+app.use((req,res,next) => {
+    res.sendFile('index.html', {root: path.join(__dirname, "dist")})
+})
+
+app.use((err,req,res,next) => {
+    console.error(err.stack)
+    res.status(500).send(
+        "<html><body><h1>500 Internal Server Error</h1><a href="/">Home</a></body></html>"
+    )
+
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
 })
+
+
